e581de470f45723e607a85e31c0d73c52e29033c
Sebastian Parisi <seba@wolflatam.com>
2013-11-20 13:30:07 -0400

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def contact
+  end
+  def we
+  end
+  def privacy	
+  end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WOLF2::Application.config.secret_token = '675c9f7c94ecfdd83413d7b20c0be67eb21ae18b14052110e127806c09d8eb26e34dbecb50fd5d806ae117b77f3db2dbd815dd9dca1741ddec78bdc6c82721bb'
