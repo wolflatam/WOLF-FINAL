3a9a228a81d55836ed7de311b4123f3bd5d3711b
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2013-12-02 11:39:38 -0300

    ActiveAdmin.routes(self) was repeated on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ WOLF2::Application.routes.draw do
 
   root :to => 'pages#home'
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 
   devise_for :users
   ActiveAdmin.routes(self)
