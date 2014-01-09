fca2e890d40407daf02697add2fb8edf5aa1ef00
sebastian Parisi <parisi89@gmail.com>
2014-01-09 18:18:46 -0400

    added analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 WOLF2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  # replace this with your tracker code
+  GA.tracker = “UA-46998732-1”
 
   # Code is not reloaded between requests
   config.cache_classes = true
